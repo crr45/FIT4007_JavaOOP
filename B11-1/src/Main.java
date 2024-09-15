b6b297a61545a42ace368a50f31f96723953ae8d
q.chung <151823410+crr45@users.noreply.github.com>
2024-09-15 16:10:23 +0700

    baitap

---
+++ B11-1/src/Main.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        for(i = 1; i <= 99; i++){
+            if(i % 2 !=0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
