7c6cdce3337c33675d9063adfc5ed780deac9bc6
vuquangchung <151823410+crr45@users.noreply.github.com>
2024-09-15 16:40:27 +0700

    Update Main.java

--- B11-1/src/Main.java
+++ B11-1/src/Main.java
@@ -3,7 +3,7 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         int i;
-        for(i = 1; i <= 100; i+2){
+        for(i = 1; i <= 100; i+=2){
             System.out.println(i);
         }
     }
