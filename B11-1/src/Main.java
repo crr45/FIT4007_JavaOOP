4eec881c26db345758b8a29ec36ecfa812e15361
q.chung <151823410+crr45@users.noreply.github.com>
2024-09-15 16:26:17 +0700

    baitap

--- B11-1/src/Main.java
+++ B11-1/src/Main.java
@@ -3,11 +3,8 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         int i;
-        for(i = 1; i <= 99; i++){
-            if(i % 2 !=0)
-            {
-                System.out.println(i);
-            }
+        for(i = 1; i <= 100; i+2){
+            System.out.println(i);
         }
     }
 }
